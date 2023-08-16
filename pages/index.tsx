841d2bccc997a3fd7e29f375a14f140ed52c97d8
khang-movember <khang.trinh@movember.com>
2023-08-16 14:29:58 +1000

    fix: removed unused lines

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import React from 'react'
+import Head from 'next/head';
 import { Button } from '../packages/mo-ui-base/src/Button/Button';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
