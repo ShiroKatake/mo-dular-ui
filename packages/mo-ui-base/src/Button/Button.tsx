cddba2af0b9ca692a1c2dfd23a729d70cf4265bb
khang-movember <khang.trinh@movember.com>
2023-11-09 10:59:54 +1100

    fix(button formatting): reduced line limit for readability

--- packages/mo-ui-base/src/Button/Button.tsx
+++ packages/mo-ui-base/src/Button/Button.tsx
@@ -1,7 +1,13 @@
 import { ButtonResetStyled, IconAfter, IconBefore, SpanContent } from './Button.styled';
 import { ButtonProps } from './Button.types';
 
-export const Button: React.FC<ButtonProps> = ({ tabIndex, children, iconBefore, iconAfter, ...otherProps }) => {
+export const Button: React.FC<ButtonProps> = ({
+  tabIndex,
+  children,
+  iconBefore,
+  iconAfter,
+  ...otherProps
+}) => {
   return (
     <ButtonResetStyled tabIndex={tabIndex} {...otherProps}>
       {iconBefore && <IconBefore>{iconBefore}</IconBefore>}
