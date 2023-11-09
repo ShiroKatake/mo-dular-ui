d51ea52998ddc4d58576fc025b7bdb7ba3251a70
khang-movember <khang.trinh@movember.com>
2023-11-09 11:04:32 +1100

    feat(nav menu): added implementation

---
+++ packages/mo-ui-base/src/NavMenu/NavMenu.tsx
@@ -0,0 +1,31 @@
+import React, { useRef } from 'react';
+import { Nav } from './NavMenu.styled';
+import { NavMenuProps } from './NavMenu.types';
+import { NavItemList } from './subcomponents/NavItemList';
+
+/**
+ * Creates a navigation menu component.
+ *
+ * @param className   - Use this to style the component.
+ * @param isVisible   - Show/hide the nav menu (hamburger toggles may find this useful).
+ * @param slotBefore  - The content to render before the navigation items.
+ * @param slotAfter   - The content to render after the navigation items.
+ * @param navLinks    - The array of navigation links to render.
+ */
+export const NavMenu: React.FC<NavMenuProps> = ({
+  className,
+  isVisible,
+  slotBefore,
+  slotAfter,
+  navLinks,
+}) => {
+  const navRef = useRef<HTMLUListElement>(null);
+
+  return (
+    <Nav ref={navRef} className={className} $isVisible={isVisible}>
+      {slotBefore}
+      <NavItemList navLinks={navLinks} />
+      {slotAfter}
+    </Nav>
+  );
+};
